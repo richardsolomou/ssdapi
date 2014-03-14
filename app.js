e4eaf210422f5a7b7b7941cbeaece5817bd35551
Richard Solomou <rsolomou@gmail.com>
2014-03-14 11:13:48 +0000

    Set up the express application.

--- app.js
+++ app.js
@@ -17,10 +17,30 @@ var port = process.env.PORT || 8080;
  * Module configuration.
  */
 
-var app = express(),
-	webapi = mysql.createConnection(config.mysql),
+var app = express();
+// Setup connection to the databases.
+var webapi = mysql.createConnection(config.mysql),
 	mssql_conn = mssql.connect(config.mssql);
 
+// Set up the express application.
+app.configure(function () {
+	// Log every request to the console.
+	app.use(express.logger('dev'));
+	// Read cookies needed for authentication.
+	app.use(express.cookieParser());
+	// Get information from HTML forms.
+	app.use(express.json());
+	app.use(express.urlencoded());
+	// Set up jade for templating.
+	app.set('view engine', 'jade');
+	// Session secret.
+	app.use(express.session({ secret: 'uopwebapi' }));
+	app.use(passport.initialize());
+	// Use persistent login sessions.
+	app.use(passport.session());
+	// Use connect-flash for flash messages stored in session.
+	app.use(flash());
+});
 
 /**
  * Route cofiguration.
