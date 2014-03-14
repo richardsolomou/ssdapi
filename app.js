5859bc2e1b9060e78761d484bbc6737c4e6cb18c
Richard Solomou <rsolomou@gmail.com>
2014-03-14 11:09:17 +0000

    Changed port and added the rest of the dependencies.

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ var express = require('express'),
 	mysql = require('mysql'),
 	mssql = require('mssql'),
 	async = require('async'),
-	db_config = require('./config');
+	passport = require('passport'),
+	flash = require('connect-flash');
+
+var port = process.env.PORT || 8080;
+	config = require('./config');
 
 
 /**
  * Module configuration.
  */
 
-var config = db_config(),
-	app = express(),
+var app = express(),
 	webapi = mysql.createConnection(config.mysql),
 	mssql_conn = mssql.connect(config.mssql);
 
@@ -29,6 +32,6 @@ app.get('/', function (req, res) {
 
 
 // Listen for connections.
-app.listen(4000, function () {
-	console.log('Listening to port 4000');
+app.listen(port, function () {
+	console.log('Listening to port ' + port);
 });
