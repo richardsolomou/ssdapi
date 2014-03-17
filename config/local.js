b7c6b3dea03209269aff772e06fc6457a1de2ffc
Richard Solomou <rsolomou@gmail.com>
2014-03-17 09:33:56 +0000

    Updated configuration data.

--- config/local.js
+++ config/local.js
@@ -1,3 +1,8 @@
 module.exports = {
-	title: 'University of Portsmouth API'
+	title: 'University of Portsmouth API',
+	version: '1',
+	version_full: '1.0.3',
+	version_date: '2014-03-17',
+	version_date_readable: 'March 17th, 2014',
+	api_url: 'http://webapi.port.ac.uk/v1'
 };
