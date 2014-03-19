9168f3437a592301f1bbb20cfa479846eadb20e7
Richard Solomou <rsolomou@gmail.com>
2014-03-19 14:27:48 +0000

    Updated the API url and version.

--- config/local.js
+++ config/local.js
@@ -1,8 +1,8 @@
 module.exports = {
 	title: 'University of Portsmouth API',
 	version: '1',
-	version_full: '1.0.3',
-	version_date: '2014-03-17',
-	version_date_readable: 'March 17th, 2014',
-	api_url: 'http://webapi.port.ac.uk/v1'
+	version_full: '0.0.2',
+	version_date: '2014-03-19',
+	version_date_readable: 'March 19th, 2014',
+	api_url: 'http://localhost:8080'
 };
