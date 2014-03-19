0a40f70ea3a5f4c1359a892b49d5787e5dc48b79
Richard Solomou <rsolomou@gmail.com>
2014-03-19 14:33:51 +0000

    Updated local configuration structure.

--- config/local.js
+++ config/local.js
@@ -1,8 +1,10 @@
 module.exports = {
 	title: 'University of Portsmouth API',
-	version: '1',
-	version_full: '0.0.2',
-	version_date: '2014-03-19',
-	version_date_readable: 'March 19th, 2014',
-	api_url: 'http://localhost:8080'
+	url: 'http://localhost:8080',
+	version: {
+		number: '1',
+		real: '0.0.2',
+		date: '2014-03-19',
+		date_readable: 'March 19th, 2014'
+	}
 };
