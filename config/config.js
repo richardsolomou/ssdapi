2d85dbd5e5abd2b47ba40a9aa116d1c7013a3d72
Richard Solomou <rsolomou@gmail.com>
2014-03-19 14:03:39 +0000

    Updated config.js to include passport and express configuration data.

--- config/config.js
+++ config/config.js
@@ -1,14 +1,23 @@
 module.exports = {
-	mysql: {
-		host: 'foo',
-		user: 'bar',
-		password: 'baz',
-		database: 'qux'
+	db: {
+		mysql: {
+			host: 'foo',
+			user: 'bar',
+			password: 'baz',
+			database: 'qux'
+		},
+		mssql: {
+			server: 'foo',
+			user: 'bar',
+			password: 'baz',
+			database: 'qux'
+		}
 	},
-	mssql: {
-		server: 'foo',
-		user: 'bar',
-		password: 'baz',
-		database: 'qux'
+	passport: {
+		client_id: 'foo',
+		client_secret: 'bar'
+	},
+	express: {
+		secret: 'baz'
 	}
 };
