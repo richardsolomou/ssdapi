e0e81fe463e3586ffca6d887e12155693b990f77
Richard Solomou <rsolomou@gmail.com>
2014-03-20 15:53:04 +0000

    Added the routes file and created the middleware for API authorization.

---
+++ app/routes.js
@@ -0,0 +1,16 @@
+module.exports = function (app, passport) {
+	// Middleware to check if the user has provided a valid API key.
+	function isAuthorized(req, res, next) {
+		// Use passport to authenticate with localapikey.
+		passport.authenticate('localapikey', function (err, user, info) {
+			if (err) return next(err);
+			// Returns an appropriate error message to the user.
+			if (!user) return res.json(401, { error: { name: info.error, message: info.message, code: 401 } });
+			// Attemps to authenticate the user.
+			req.logIn(user, function (err) {
+				if (err) return next(err);
+				return next();
+			});
+		})(req, res, next);
+	}
+};
