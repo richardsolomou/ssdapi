4e002e9d09af86e56777809e5e6c2a64e0f4b8d1
Richard Solomou <rsolomou@gmail.com>
2014-03-21 13:12:00 +0000

    Updated local configuration.

--- config/local.js
+++ config/local.js
@@ -1,10 +1,13 @@
-module.exports = {
-	title: 'University of Portsmouth API',
-	url: 'http://localhost:8080',
-	version: {
-		number: '1',
-		real: '0.0.2',
-		date: '2014-03-19',
-		date_readable: 'March 19th, 2014'
-	}
-};
+var config = module.exports = {};
+
+config.title = 'University of Portsmouth API';
+
+config.version = {};
+config.version.number = '1';
+config.version.real_number = '0.0.2';
+config.version.date = '2014-03-21';
+config.version.date_readable = 'March 21st, 2014';
+
+config.api = {}
+config.api.base_url = 'http://localhost:8080';
+config.api.full_url = config.api.base_url + '/v' + config.version.number + '/';
