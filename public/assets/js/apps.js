0d64390e57de65833701f176ed04f1bcdd017ff1
Richard Solomou <rsolomou@gmail.com>
2014-03-31 11:46:40 +0100

    Added functionality for creating, updating and deleting apps with modals.

--- public/assets/js/apps.js
+++ public/assets/js/apps.js
@@ -4,15 +4,31 @@
 $(document).ready(function () {
 
 	$(document).on('click', '[data-action=delete]', function () {
-		var app = $(this).closest('.app'),
-			id = app.data('id');
+		var id = $(this).closest('.app').data('id');
+		$('#delete_id').val(id);
+	});
+
+	$(document).on('click', '[data-action=update]', function () {
+		var app = $(this).closest('.app');
+		var id = app.data('id'),
+			title = app.find('[data-info=title] code').text(),
+			request_origin = app.find('[data-info=request_origin] code').text();
+
+		$('#update_id').val(id);
+		$('#update_title').val(title);
+		$('#update_request_origin').val(request_origin);
+	});
+
+	$(document).on('click', '[data-action=doDelete]', function () {
+		var id = $('#delete_id').val();
 
 		$.ajax({
 			type: 'DELETE',
 			url: '/apps',
 			data: { id: id },
 			success: function (data) {
-				app.fadeOut('slow', function () {
+				$('#deleteApp').modal('hide');
+				$('[data-id=' + id + ']').fadeOut('slow', function () {
 					$(this).remove();
 				});
 			}
@@ -21,14 +37,14 @@ $(document).ready(function () {
 		return false;
 	});
 
-	$(document).on('click', '[data-action=create]', function () {
-		var app = $(this).closest('.app');
-
+	$(document).on('click', '[data-action=doCreate]', function () {
 		$.ajax({
 			type: 'POST',
 			url: '/apps',
-			data: { title: $('#title').val(), request_origin: $('#request_origin').val() },
+			data: { title: $('#new_title').val(), request_origin: $('#new_request_origin').val() },
 			success: function (data) {
+				$('#newApp').modal('hide');
+
 				$('<div/>', { 'class': 'app', 'data-id': data.id }).append(
 					$('<table/>', { 'class': 'table' }).append(
 						$('<tr/>').append(
@@ -41,17 +57,40 @@ $(document).ready(function () {
 						),
 						$('<tr/>').append(
 							$('<th/>').text('Title'),
-							$('<td/>').html($('<code/>').text(data.title))
+							$('<td/>', { 'data-info': 'title' }).html($('<code/>').text(data.title))
 						),
 						$('<tr/>').append(
 							$('<th/>').text('Request Origin'),
-							$('<td/>').html($('<code/>').text(data.request_origin))
+							$('<td/>', { 'data-info': 'request_origin' }).html($('<code/>').text(data.request_origin))
 						)
 					),
-					$('<a/>', { 'href': '#', 'class': 'btn btn-default', 'data-action': 'update' }).text('Update').prepend(' ').prepend($('<i/>', { 'class': 'fa fa-edit' })),
-					$('<a/>', { 'href': '#', 'class': 'btn btn-default', 'data-action': 'download_json' }).text('Download JSON').prepend(' ').prepend($('<i/>', { 'class': 'fa fa-download' })),
-					$('<a/>', { 'href': '#', 'class': 'btn btn-warning', 'data-action': 'delete' }).text('Delete').prepend(' ').prepend($('<i/>', { 'class': 'fa fa-times-circle' }))
+					$('<button/>', { 'type': 'button', 'class': 'btn btn-default', 'data-toggle': 'modal', 'data-target': '#updateApp', 'data-action': 'update' }).text('Update').prepend(' ').prepend($('<i/>', { 'class': 'fa fa-edit' })),
+					$('<button/>', { 'type': 'button', 'class': 'btn btn-default', 'data-toggle': 'modal', 'data-target': '#downloadJson', 'data-action': 'download_json' }).text('Download JSON').prepend(' ').prepend($('<i/>', { 'class': 'fa fa-download' })),
+					$('<button/>', { 'type': 'button', 'class': 'btn btn-warning', 'data-toggle': 'modal', 'data-target': '#deleteApp', 'data-action': 'delete' }).text('Delete').prepend(' ').prepend($('<i/>', { 'class': 'fa fa-times-circle' }))
 				).appendTo('#apps');
+			}
+		});
+
+		return false;
+	});
+
+	$(document).on('click', '[data-action=doUpdate]', function () {
+		var id = $('#update_id').val();
+		var app = $('[data-id=' + id + ']');
+		var title = app.find('[data-info=title] code'),
+			request_origin = app.find('[data-info=request_origin] code'),
+			new_title = $('#update_title').val(),
+			new_request_origin = $('#update_request_origin').val();
+
+		$.ajax({
+			type: 'PUT',
+			url: '/apps',
+			data: { id: id, title: new_title, request_origin: new_request_origin },
+			success: function (data) {
+				$('#updateApp').modal('hide');
+
+				title.text(new_title);
+				request_origin.text(new_request_origin);
 			}
 		});
 
