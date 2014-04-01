8f81c6f62a81ef63cace3c3d522d83119a47cd23
Richard Solomou <rsolomou@gmail.com>
2014-04-01 10:10:45 +0100

    Updated API routes to work with JSONP.

--- app/api.js
+++ app/api.js
@@ -7,10 +7,10 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get a specific user.
 		mysql.query('SELECT `title`, `first_name`, `last_name`, `job_title`, `building`, `department`, `extension`, `email`, `username`, `section`, `faculty` FROM `users` WHERE `username` = :username', { username: req.params.username }, function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Invalid username.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Invalid username.', code: 404 } });
 			// Return the user in JSON format.
-			return res.json(results[0]);
+			return res.jsonp(results[0]);
 		});
 	});
 
@@ -19,10 +19,10 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get all users.
 		mysql.query('SELECT `title`, `first_name`, `last_name`, `job_title`, `building`, `department`, `extension`, `email`, `username`, `section`, `faculty` FROM `users`', function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Users table is empty.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Users table is empty.', code: 404 } });
 			// Return all users in JSON format.
-			return res.json(results);
+			return res.jsonp(results);
 		});
 	});
 
@@ -31,10 +31,10 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get all service status problems.
 		mysql.query('SELECT `name`, `info`, `description`, `status`, `modified_date`, `type`, `service_manager`, `main_link_name` FROM `services`', function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Service status table is empty.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Service status table is empty.', code: 404 } });
 			// Return all service status problems in JSON format.
-			return res.json(results);
+			return res.jsonp(results);
 		});
 	});
 
@@ -43,10 +43,10 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get the timetables for a specific lab.
 		mysql.query('SELECT `lecturer`, `start_time`, `finish_time`, `module_name`, `module_type` FROM `timetables` INNER JOIN `buildings` ON `buildings`.`id` = `timetables`.`building_id` AND `buildings`.`reference` = :reference INNER JOIN `labs` ON `labs`.`id` = `timetables`.`lab_id` AND `labs`.`short_identifier` = :short_identifier', { reference: req.params.reference, short_identifier: req.params.short_identifier }, function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'There are no timetables for this building.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'There are no timetables for this building.', code: 404 } });
 			// Return the timetables in JSON format.
-			return res.json(results);
+			return res.jsonp(results);
 		});
 	});
 
@@ -55,8 +55,8 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get the timetables for a specific building.
 		mysql.query('SELECT `lecturer`, `start_time`, `finish_time`, `module_name`, `module_type`, `lab_id` FROM `timetables` INNER JOIN `buildings` ON `buildings`.`id` = `timetables`.`building_id` AND `buildings`.`reference` = :reference', { reference: req.params.reference }, function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'There are no timetables for this building.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'There are no timetables for this building.', code: 404 } });
 			// Loop through all the timetables.
 			async.each(results, function (timetable, callback) {
 				// Run a MySQL query to get the lab details for the timetable.
@@ -71,7 +71,7 @@ module.exports = function (app, passport, mysql, mssql) {
 			// Execute a function after the buildings loop has finished.
 			}, function () {
 				// Return timetables timetables in JSON format.
-				return res.json(results);
+				return res.jsonp(results);
 			});
 		});
 	});
@@ -81,8 +81,8 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get the timetables.
 		mysql.query('SELECT * FROM `timetables`', function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Timetables table is empty.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Timetables table is empty.', code: 404 } });
 			// Loop through all the timetables.
 			async.each(results, function (timetable, callback) {
 				// Run a series of functions in parallel for each timetable.
@@ -121,7 +121,7 @@ module.exports = function (app, passport, mysql, mssql) {
 			// Execute a function after the buildings loop has finished.
 			}, function () {
 				// Return the timetables in JSON format.
-				return res.json(results);
+				return res.jsonp(results);
 			});
 		});
 	});
@@ -131,10 +131,10 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get the lab.
 		mysql.query('SELECT `labs`.`short_identifier`, `labs`.`room_number` FROM `labs` INNER JOIN `buildings` ON `buildings`.`id` = `labs`.`building_id` AND `buildings`.`reference` = :reference WHERE `labs`.`short_identifier` = :short_identifier', { reference: req.params.reference, short_identifier: req.params.short_identifier }, function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Invalid lab short identifier.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Invalid lab short identifier.', code: 404 } });
 			// Return the lab in JSON format.
-			return res.json(results[0]);
+			return res.jsonp(results[0]);
 		});
 	});
 
@@ -143,10 +143,10 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get the shared teaching spaces of the building.
 		mysql.query('SELECT `labs`.`short_identifier`, `labs`.`room_number` FROM `labs` INNER JOIN `buildings` ON `buildings`.`id` = `labs`.`building_id` AND `buildings`.`reference` = :reference', { reference: req.params.reference }, function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'There are no labs available for this building.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'There are no labs available for this building.', code: 404 } });
 			// Return all labs in JSON format.
-			return res.json(results);
+			return res.jsonp(results);
 		});
 	});
 
@@ -155,8 +155,8 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get all labs.
 		mysql.query('SELECT `short_identifier`, `room_number`, `building_id` FROM `labs`', function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Labs table is empty.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Labs table is empty.', code: 404 } });
 			// Loop through all the labs.
 			async.each(results, function (lab, callback) {
 				// Run a MySQL query to get the labs for the building.
@@ -171,7 +171,7 @@ module.exports = function (app, passport, mysql, mssql) {
 			// Execute a function after the buildings loop has finished.
 			}, function () {
 				// Return all labs in JSON format.
-				return res.json(results);
+				return res.jsonp(results);
 			});
 		});
 	});
@@ -181,10 +181,10 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get the opening times of all buildings.
 		mysql.query('SELECT `openingtimes`.`day_of_the_week`, `openingtimes`.`opening_time`, `openingtimes`.`closing_time` FROM `openingtimes` INNER JOIN `buildings` ON `buildings`.`id` = `openingtimes`.`building_id` AND `buildings`.`reference` = :reference', { reference: req.params.reference }, function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Opening times are not available for this building.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Opening times are not available for this building.', code: 404 } });
 			// Return all buildings in JSON format.
-			return res.json(results);
+			return res.jsonp(results);
 		});
 	});
 
@@ -194,8 +194,8 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get the opening times of all buildings.
 		mysql.query('SELECT `building_id`, `day_of_the_week`, `opening_time`, `closing_time` FROM `openingtimes`', function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Buildings table is empty.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Buildings table is empty.', code: 404 } });
 			// Loop through all the opening times.
 			async.each(results, function (openingtimes, callback) {
 				// Run a MySQL query for getting the building details.
@@ -210,7 +210,7 @@ module.exports = function (app, passport, mysql, mssql) {
 			// Execute a function after the buildings loop has finished.
 			}, function () {
 				// Return all buildings and their opening times in JSON format.
-				return res.json(results);
+				return res.jsonp(results);
 			});
 		});
 	});
@@ -234,8 +234,8 @@ module.exports = function (app, passport, mysql, mssql) {
 			// Run a MySQL query to get all the buildings that have open access areas.
 			mysql.query('SELECT `buildings`.`id`, `buildings`.`name`, `buildings`.`reference` FROM `buildings` INNER JOIN `openaccess` ON `buildings`.`id` = `openaccess`.`building_id` ' + where + ' GROUP BY `buildings`.`id`', function (err, results) {
 				// Return appropriate error messages if something went wrong.
-				if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-				if (!results || !results.length) return res.json(404, { error: { message: 'Building does not contain an open access area.', code: 404 } });
+				if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+				if (!results || !results.length) return res.jsonp(404, { error: { message: 'Building does not contain an open access area.', code: 404 } });
 				// Set an empty array for the data to be returned and sets the current date and time.
 				var data = [], d = new Date(), today = d.getDay() - 1, time = d.toLocaleTimeString(), days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 				// Run a series of functions for each building returned.
@@ -307,7 +307,7 @@ module.exports = function (app, passport, mysql, mssql) {
 						delete data.building;
 					}
 					// Return the open access area availability data.
-					return res.json(data);
+					return res.jsonp(data);
 				});
 			});
 		});
@@ -318,10 +318,10 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get a specific building.
 		mysql.query('SELECT `name`, `reference`, `latitude`, `longitude` FROM `buildings` WHERE `reference` = :reference', { reference: req.params.reference }, function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Invalid building reference.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Invalid building reference.', code: 404 } });
 			// Return a specific building in JSON format.
-			return res.json(results[0]);
+			return res.jsonp(results[0]);
 		});
 	});
 
@@ -330,10 +330,10 @@ module.exports = function (app, passport, mysql, mssql) {
 		// Run a MySQL query to get all buildings.
 		mysql.query('SELECT `name`, `reference`, `latitude`, `longitude` FROM `buildings`', function (err, results) {
 			// Return appropriate error messages if something went wrong.
-			if (err) return res.json(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
-			if (!results || !results.length) return res.json(404, { error: { message: 'Buildings table is empty.', code: 404 } });
+			if (err) return res.jsonp(500, { error: { message: 'Something went wrong.', code: 500, details: err } });
+			if (!results || !results.length) return res.jsonp(404, { error: { message: 'Buildings table is empty.', code: 404 } });
 			// Return all buildings in JSON format.
-			return res.json(results);
+			return res.jsonp(results);
 		});
 	});
 
@@ -343,7 +343,7 @@ module.exports = function (app, passport, mysql, mssql) {
 		passport.authenticate('localapikey', function (err, user, info) {
 			if (err) return next(err);
 			// Return an appropriate error message to the user.
-			if (!user) return res.json(401, { error: { name: info.error, message: info.message, code: 401 } });
+			if (!user) return res.jsonp(401, { error: { name: info.error, message: info.message, code: 401 } });
 			// Attempt to authenticate the user.
 			req.logIn(user, function (err) {
 				if (err) return next(err);
