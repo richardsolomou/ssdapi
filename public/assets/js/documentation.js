b48584c429468b1a49aa85b1e74874f29081eb15
Richard Solomou <rsolomou@gmail.com>
2014-04-04 11:41:26 +0100

    Fixed scrollspy in documentation and added string identifiers section.

--- public/assets/js/documentation.js
+++ public/assets/js/documentation.js
@@ -4,6 +4,7 @@
 $(document).ready(function () {
 
 	var api = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'));
+	var access_token = '8ac61297-c3c5-4f76-bc6e-603471ff9325';
 
 	$('[data-toggle=popover]').popover({ html: true });
 
@@ -11,27 +12,27 @@ $(document).ready(function () {
 		$('pre[data-example-request]').each(function () {
 			var request = $(this).data('example-request');
 			var pre = $(this);
-			var access_token = '8ac61297-c3c5-4f76-bc6e-603471ff9325';
 			
 			request += request.indexOf('?') !== -1 ? '&' : '?';
 			request += 'access_token=' + access_token;
 
 			$.getJSON(api + '/v1/' + request).complete(function (data) {
 				pre.text(JSON.stringify(data.responseJSON, null, '\t'));
+				$('body').scrollspy('refresh');
 			});
+
 		});
 
-		/*$.getJSON(api + '/v1/buildings', function (data) {
+		$.getJSON(api + '/v1/buildings?access_token=' + access_token, function (data) {
 			for (var i = 0; i < data.length; i++) {
-				$('#documentation_references_string_identifiers table tbody').append(
+				$('#documentation_string_identifiers table tbody').append(
 					'<tr>' +
-						'<td>' + data[i].id + '</td>' +
-						'<td><code>' + data[i].reference + '</code></td>' +
 						'<td>' + data[i].name + '</td>' +
+						'<td><code>' + data[i].reference + '</code></td>' +
 					'</tr>'
 				);
 			}
-		});*/
+		});
 	
 		$(window).off('.affix');
 		$('#doc_nav').removeData('bs.affix').removeData('affix').removeAttr('class').affix({
