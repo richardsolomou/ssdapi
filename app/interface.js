f40f62263c0625f400e63acef9c680f4a98641bd
Richard Solomou <rsolomou@gmail.com>
2014-04-04 10:27:53 +0100

    Prepended api folder in URLs and added ordering of apps via creation date.

--- app/interface.js
+++ app/interface.js
@@ -14,9 +14,9 @@ module.exports = function (app, passport, mysql) {
 		// Create a variable with an error flash message.
 		var error = req.flash('error') || [];
 		// Use passport to check if the user is already authenticated.
-		if (req.isAuthenticated() && req.session.user) return res.redirect('/');
+		if (req.isAuthenticated() && req.session.user) return res.redirect('/api');
 		// Check that there isn't an error.
-		if (error.length == 0) return res.redirect('/auth/google');
+		if (error.length == 0) return res.redirect('/api/auth/google');
 		// Render login.ejs with any potential user credentials and error messages.
 		res.render('login', { user: req.session.user, error: error });
 	});
@@ -30,7 +30,7 @@ module.exports = function (app, passport, mysql) {
 	// Route to show all the user's apps.
 	app.get('/apps', isLoggedIn, function (req, res) {
 		// Run a MySQL query to get all the apps for the current user.
-		mysql.query('SELECT * FROM `api_apps` WHERE `user_id` = :user_id', { user_id: req.session.user.id }, function (err, apps) {
+		mysql.query('SELECT * FROM `api_apps` WHERE `user_id` = :user_id ORDER BY `created_at` DESC', { user_id: req.session.user.id }, function (err, apps) {
 			// Render apps.ejs with the user's apps.
 			res.render('apps', { name: 'apps', user: req.session.user, apps: apps, css: 'apps', js: 'apps' });
 		});
@@ -62,12 +62,11 @@ module.exports = function (app, passport, mysql) {
 			});
 		// Run a function after the callback was called.
 		}, function (err) {
-			var data = { id: id, user_id: req.session.user.id, access_token: access_token, title: req.body.title, request_origin: req.body.request_origin };
+			// Set the data to be passed.
+			var data = { id: id, user_id: req.session.user.id, access_token: access_token, title: req.body.title, request_origin: req.body.request_origin, created_at: new Date().toISOString().slice(0, 19).replace('T', ' ') };
 			// Run a MySQL query for creating a new app.
-			mysql.query('INSERT INTO `api_apps` (`id`, `user_id`, `access_token`, `title`, `request_origin`) VALUES (:id, :user_id, :access_token, :title, :request_origin)', data, function (err, apps) {
+			mysql.query('INSERT INTO `api_apps` (`id`, `user_id`, `access_token`, `title`, `request_origin`, `created_at`) VALUES (:id, :user_id, :access_token, :title, :request_origin, :created_at)', data, function (err, apps) {
 				// Return the data to the view.
-				console.dir(err);
-				console.dir(apps);
 				res.json(data);
 			});
 		});
@@ -101,7 +100,7 @@ module.exports = function (app, passport, mysql) {
 		// Use passport to logout user.
 		req.logout();
 		// Redirect user to homepage.
-		res.redirect('/');
+		res.redirect('/api');
 	});
 
 	// Route for displaying the API documentation.
@@ -116,9 +115,9 @@ module.exports = function (app, passport, mysql) {
 	// Route for the google authentication callback.
 	app.get('/auth/google/callback', passport.authenticate('google', {
 		// Set redirect URL on successful log-in.
-		successRedirect: '/',
+		successRedirect: '/api',
 		// Set redirect URL on failed log-in.
-		failureRedirect: '/login'
+		failureRedirect: '/api/login'
 	}));
 };
 
@@ -127,5 +126,5 @@ function isLoggedIn(req, res, next) {
 	// Use passport to check if authenticated.
 	if (req.isAuthenticated() && req.session.user) return next();
 	// Redirect to login page if not.
-	res.redirect('/auth/google');
+	res.redirect('/api/auth/google');
 }
