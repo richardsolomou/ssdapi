bca036746738feb51219cf6c9daf9848d315ef5a
Richard Solomou <rsolomou@gmail.com>
2014-04-16 09:06:11 +0100

    Renamed service to Student Service Delivery API.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#[![](http://dsign-web-01.uni.ds.port.ac.uk/api/assets/img/api.png)](http://dsign-web-01.uni.ds.port.ac.uk/api)
+#[![](http://ssd.api.port.ac.uk/assets/img/logo.png)](http://ssd.api.port.ac.uk/)
 
-The University of Portsmouth API is a RESTful API linked to multiple extensive databases around the University of Portsmouth that displays all kinds of data to allow developers to interact with and create their own applications using data from the University of Portsmouth.
+The Student Service Delivery API is a RESTful API linked to multiple extensive databases around the University of Portsmouth that displays all kinds of data to allow developers to interact with and create their own applications using data from the University of Portsmouth.
--- config/local.js
+++ config/local.js
@@ -1,7 +1,7 @@
 var config = module.exports = {};
 
 // Name of the application.
-config.title = 'University of Portsmouth API';
+config.title = 'Student Service Delivery API';
 
 
 /**
@@ -12,11 +12,11 @@ config.version = {};
 // Official version number as shown in the URL.
 config.version.number = '1';
 // Real version number with increments.
-config.version.real_number = '0.0.7';
+config.version.real_number = '0.0.8';
 // Last version update in SQL format.
-config.version.date = '2014-04-09';
+config.version.date = '2014-04-16';
 // Last version update (readable).
-config.version.date_readable = 'April 9th, 2014';
+config.version.date_readable = 'April 16th, 2014';
 
 
 /**
@@ -25,10 +25,8 @@ config.version.date_readable = 'April 9th, 2014';
 config.api = {}
 
 // Hostname of the application.
-config.api.hostname = 'dsign-web-01.uni.ds.port.ac.uk';
-// Folder of the application.
-config.api.folder = '/api';
+config.api.hostname = 'ssd.api.port.ac.uk';
 // Base URL of the application.
-config.api.base_url = 'http://' + config.api.hostname + config.api.folder;
+config.api.base_url = 'http://' + config.api.hostname;
 // API URL including version number and trailing slash.
 config.api.full_url = config.api.base_url + '/v' + config.version.number + '/';
