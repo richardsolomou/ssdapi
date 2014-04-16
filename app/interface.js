6882bcb5390b72c0f6c080a63fe7a4cbe1aeb461
Richard Solomou <rsolomou@gmail.com>
2014-04-16 09:08:49 +0100

    Removed global folder variable and upgraded apps page to ExpressJS 4.0 standards.

--- app/interface.js
+++ app/interface.js
@@ -15,9 +15,9 @@ module.exports = function (app, passport, mysql, local) {
 		// Create a variable with an error flash message.
 		var error = req.flash('error') || [];
 		// Use passport to check if the user is already authenticated.
-		if (req.isAuthenticated() && req.session.user) return res.redirect(local.api.folder);
+		if (req.isAuthenticated() && req.session.user) return res.redirect('/');
 		// Check that there isn't an error.
-		if (error.length == 0) return res.redirect(local.api.folder + '/auth/google');
+		if (error.length == 0) return res.redirect('/auth/google');
 		// Render login.ejs with any potential user credentials and error messages.
 		res.render('login', { user: req.session.user, error: error });
 	});
@@ -28,83 +28,85 @@ module.exports = function (app, passport, mysql, local) {
 		res.render('documentation', { name: 'documentation', user: req.session.user });
 	});
 
-	// Route to show all the user's apps.
-	app.get('/apps', isLoggedIn, function (req, res) {
-		// Run a MySQL query to get all the apps for the current user.
-		mysql.query('SELECT * FROM `api_apps` WHERE `user_id` = :user_id ORDER BY `created_at` DESC', { user_id: req.session.user.id }, function (err, apps) {
-			// Render apps.ejs with the user's apps.
-			res.render('apps', { name: 'apps', user: req.session.user, apps: apps });
-		});
-	});
-
-	// Route for creating a new application under the user's account.
-	app.post('/apps', isLoggedIn, function (req, res) {
-		// Checks if the title is empty.
-		if (validator.equals(req.body.title, '')) return res.json(403, { error: { message: 'Title cannot be empty.', code: 403 } });
-		// Checks if the request origin is empty.
-		if (validator.equals(req.body.request_origin, '')) return res.json(403, { error: { message: 'Request origin cannot be empty.', code: 403 } });
-		// Checks if the request origin is a valid URL or IP.
-		if (!validator.isURL(req.body.request_origin) && !validator.isIP(req.body.request_origin) || validator.contains(req.body.request_origin, '*') || req.body.request_origin.slice(-1) == '/') return res.json(403, { error: { message: 'Request origin is not a valid URL or IP address.', code: 403 } });
-		// Create an access token variable.
-		var id = access_token = null;
-		// Run an asynchronous loop.
-		async.whilst(function () {
-			// Run a loop while the access token variable is empty.
-			return access_token == null;
-		// Call a function whilst the above function returns true.
-		}, function (callback) {
-			// Generate an id.
-			id = uuid.v1();
-			// Generate an access token.
-			var token = uuid.v4();
-			// Run a MySQL query to check if the access token already exists.
-			mysql.query('SELECT * FROM `api_apps` WHERE `access_token` IN [:id, :access_token] OR `id` IN [:id, :access_token]', { id: id, access_token: token }, function (err, results) {
-				// Check if no results were found.
-				if (!results || !results.length) {
-					// Assign the token to the access token.
-					access_token = token;
-					// Call the callback function to continue.
-					callback();
-				}
+	// Route to handle HTTP verbs for apps.
+	app.route('/apps')
+		// Matches all HTTP verbs for this route.
+		.all(isLoggedIn)
+		// Route to show all the user's apps.
+		.get(function (req, res) {
+			// Run a MySQL query to get all the apps for the current user.
+			mysql.query('SELECT * FROM `api_apps` WHERE `user_id` = :user_id ORDER BY `created_at` DESC', { user_id: req.session.user.id }, function (err, apps) {
+				// Render apps.ejs with the user's apps.
+				res.render('apps', { name: 'apps', user: req.session.user, apps: apps });
 			});
-		// Run a function after the callback was called.
-		}, function (err) {
-			// Set the data to be passed.
-			var data = { id: id, user_id: req.session.user.id, access_token: access_token, title: req.body.title, request_origin: req.body.request_origin, created_at: new Date().toISOString().slice(0, 19).replace('T', ' ') };
+		})
+		// Route for creating a new application under the user's account.
+		.post(function (req, res) {
+			// Checks if the title is empty.
+			if (validator.equals(req.body.title, '')) return res.json(403, { error: { message: 'Title cannot be empty.', code: 403 } });
+			// Checks if the request origin is empty.
+			if (validator.equals(req.body.request_origin, '')) return res.json(403, { error: { message: 'Request origin cannot be empty.', code: 403 } });
+			// Checks if the request origin is a valid URL or IP.
+			if (!validator.isURL(req.body.request_origin) && !validator.isIP(req.body.request_origin) || validator.contains(req.body.request_origin, '*') || req.body.request_origin.slice(-1) == '/') return res.json(403, { error: { message: 'Request origin is not a valid URL or IP address.', code: 403 } });
+			// Create an access token variable.
+			var id = access_token = null;
+			// Run an asynchronous loop.
+			async.whilst(function () {
+				// Run a loop while the access token variable is empty.
+				return access_token == null;
+			// Call a function whilst the above function returns true.
+			}, function (callback) {
+				// Generate an id.
+				id = uuid.v1();
+				// Generate an access token.
+				var token = uuid.v4();
+				// Run a MySQL query to check if the access token already exists.
+				mysql.query('SELECT * FROM `api_apps` WHERE `access_token` IN [:id, :access_token] OR `id` IN [:id, :access_token]', { id: id, access_token: token }, function (err, results) {
+					// Check if no results were found.
+					if (!results || !results.length) {
+						// Assign the token to the access token.
+						access_token = token;
+						// Call the callback function to continue.
+						callback();
+					}
+				});
+			// Run a function after the callback was called.
+			}, function (err) {
+				// Set the data to be passed.
+				var data = { id: id, user_id: req.session.user.id, access_token: access_token, title: req.body.title, request_origin: req.body.request_origin, created_at: new Date().toISOString().slice(0, 19).replace('T', ' ') };
+				// Run a MySQL query for creating a new app.
+				mysql.query('INSERT INTO `api_apps` (`id`, `user_id`, `access_token`, `title`, `request_origin`, `created_at`) VALUES (:id, :user_id, :access_token, :title, :request_origin, :created_at)', data, function (err, apps) {
+					// Return the data to the view.
+					res.json(data);
+				});
+			});
+		})
+		// Route for modifying an existing application under the user's account.
+		.put(function (req, res) {
+			// Checks if the title is empty.
+			if (validator.equals(req.body.title, '')) return res.json(403, { error: { message: 'Title cannot be empty.', code: 403 } });
+			// Checks if the request origin is empty.
+			if (validator.equals(req.body.request_origin, '')) return res.json(403, { error: { message: 'Request origin cannot be empty.', code: 403 } });
+			// Checks if the request origin is a valid URL or IP.
+			if (!validator.isURL(req.body.request_origin) && !validator.isIP(req.body.request_origin) || validator.contains(req.body.request_origin, '*') || req.body.request_origin.slice(-1) == '/') return res.json(403, { error: { message: 'Request origin is not a valid URL or IP address.', code: 403 } });
+			// Get the App ID.
+			var id = req.body.id;
+			// Run a MySQL query to update the app.
+			mysql.query('UPDATE `api_apps` SET `title` = :title, `request_origin` = :request_origin WHERE `id` = :id', { id: req.body.id, title: req.body.title, request_origin: req.body.request_origin }, function (err, results) {
+				// Return the results to the view.
+				res.json(results);
+			});
+		})
+		// Route for deleting an existing application.
+		.delete(function (req, res) {
+			// Get the App ID.
+			var id = req.body.id;
 			// Run a MySQL query for creating a new app.
-			mysql.query('INSERT INTO `api_apps` (`id`, `user_id`, `access_token`, `title`, `request_origin`, `created_at`) VALUES (:id, :user_id, :access_token, :title, :request_origin, :created_at)', data, function (err, apps) {
-				// Return the data to the view.
-				res.json(data);
+			mysql.query('DELETE FROM `api_apps` WHERE `id` = :id', { id: id }, function (err, results) {
+				// Return the results to the view.
+				res.json(results);
 			});
 		});
-	});
-
-	// Route for modifying an existing application under the user's account.
-	app.put('/apps', isLoggedIn, function (req, res) {
-		// Checks if the title is empty.
-		if (validator.equals(req.body.title, '')) return res.json(403, { error: { message: 'Title cannot be empty.', code: 403 } });
-		// Checks if the request origin is empty.
-		if (validator.equals(req.body.request_origin, '')) return res.json(403, { error: { message: 'Request origin cannot be empty.', code: 403 } });
-		// Checks if the request origin is a valid URL or IP.
-		if (!validator.isURL(req.body.request_origin) && !validator.isIP(req.body.request_origin) || validator.contains(req.body.request_origin, '*') || req.body.request_origin.slice(-1) == '/') return res.json(403, { error: { message: 'Request origin is not a valid URL or IP address.', code: 403 } });
-		// Get the App ID.
-		var id = req.body.id;
-		// Run a MySQL query to update the app.
-		mysql.query('UPDATE `api_apps` SET `title` = :title, `request_origin` = :request_origin WHERE `id` = :id', { id: req.body.id, title: req.body.title, request_origin: req.body.request_origin }, function (err, results) {
-			// Return the results to the view.
-			res.json(results);
-		});
-	});
-
-	app.delete('/apps', isLoggedIn, function (req, res) {
-		// Get the App ID.
-		var id = req.body.id;
-		// Run a MySQL query for creating a new app.
-		mysql.query('DELETE FROM `api_apps` WHERE `id` = :id', { id: id }, function (err, results) {
-			// Return the results to the view.
-			res.json(results);
-		});
-	});
 
 	// Route for logging out of the API.
 	app.get('/logout', isLoggedIn, function (req, res) {
@@ -113,7 +115,7 @@ module.exports = function (app, passport, mysql, local) {
 		// Use passport to logout user.
 		req.logout();
 		// Redirect user to homepage.
-		res.redirect(local.api.folder);
+		res.redirect('/');
 	});
 
 	// Route for google authentication.
@@ -122,9 +124,9 @@ module.exports = function (app, passport, mysql, local) {
 	// Route for the google authentication callback.
 	app.get('/auth/google/callback', passport.authenticate('google', {
 		// Set redirect URL on successful log-in.
-		successRedirect: local.api.folder,
+		successRedirect: '/',
 		// Set redirect URL on failed log-in.
-		failureRedirect: local.api.folder + '/login'
+		failureRedirect: '/login'
 	}));
 	
 	// Middleware to check if the user is logged in.
@@ -132,6 +134,6 @@ module.exports = function (app, passport, mysql, local) {
 		// Use passport to check if authenticated.
 		if (req.isAuthenticated() && req.session.user) return next();
 		// Redirect to login page if not.
-		res.redirect(local.api.folder + '/auth/google');
+		res.redirect('/auth/google');
 	}
 };
